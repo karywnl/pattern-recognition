e655df8b6bd0fe0de2c8055a4232f57d8e5f7bec
kary <karywnl@gmail.com>
2026-07-19 14:55:47 +0530

    added transpose, matmult to linalg module

--- src/linalg.py
+++ src/linalg.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 # dot product
 def dot(a, b):
@@ -6,8 +7,31 @@ def dot(a, b):
         tot += a[i] * b[i]
     return tot    
 
+# transpose mxn to nxm
+def transpose(A):
+    m = len(A)
+    n = len(A[0])
 
+    arr = np.zeros((n, m))
+    for i in range(n):
+        for j in range(m):
+            arr[i, j] = A[j][i]
 
-
+    return arr
 
 # matrix multiplication
+def matmul(A, B):
+    # A (mxn) * B (nxk) = C (mxk)
+    m = len(A)
+    n = len(B)
+    k = len(B[0])
+
+    B_t = transpose(B)
+    arr = np.zeros((m, k))
+
+    for i in range(m):
+        for j in range(k):
+            arr[i, j] = dot(A[i], B_t[j])
+
+    return arr
+
